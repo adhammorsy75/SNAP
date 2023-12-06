f127a3aaed17e399f9508a6e93736546c0eedf48
Adham <Adhammorsy75@gmail.com>
2023-12-06 10:43:40 -0500

    update

--- SNAP/models/Index.js
+++ SNAP/models/Index.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose')
 const cartSchema = require('./Cart.js')
-// const gamesSchema = require('./Games.js')
+const gamesSchema = require('./Games.js')
 const userSchema = require('./User.js')
 
 
 const Cart = mongoose.model('Cart', cartSchema)
-// const Games = mongoose.model('Games', gamesSchema)
+const Games = mongoose.model('Games', gamesSchema)
 const User = mongoose.model('User', userSchema)
 
 module.exports = {
     Cart,
-    // Games,
+    Games,
     User,
     
 }
